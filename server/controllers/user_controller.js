a10d1334ff7915a10858545cb68037fe97e943d2
ironyhouse <ironylait@gmail.com>
2023-03-27 21:01:59 +0200

    BE: add routs and router controller

---
+++ server/controllers/user_controller.js
@@ -0,0 +1,34 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+
+  async login(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+
+  async logout(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+
+  async activate(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+
+  async refresh(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+
+  async gerUsers(req, res, next) {
+    try {
+      res.json(['asd', 'asd']);
+    } catch (e) {}
+  }
+}
+
+module.exports = new UserController();
--- server/router/index.js
+++ server/router/index.js
@@ -1,13 +1,13 @@
 const Router = require('express').Router;
-
+const userController = require('../controllers/user_controller');
 const router = new Router();
 
 // endpoints
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
-router.get('/refresh');
-router.get('/users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.gerUsers);
 
 module.exports = router;
