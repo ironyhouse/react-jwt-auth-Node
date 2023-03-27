6cf691bb1d7e7a2abbe2c3b5a291146753e33da8
ironyhouse <ironylait@gmail.com>
2023-03-27 19:59:24 +0200

    add mongo database connection and middleware

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
-const PORT = 5000;
+// add default PORT
+const PORT = process.env.PORT || 5000;
 // create app instance
 const app = express();
+
+// add middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 // run server
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log(`Server started on PORT = ${5000}`));
+    // connect to data base
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);
   }
