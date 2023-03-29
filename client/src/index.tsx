0c10d1794031ab8feaa3289e3b5d46b6a046d052
ironyhouse <ironylait@gmail.com>
2023-03-29 17:27:30 +0200

    add store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store from './store/Store';
+
+interface StoreInterface {
+  store: Store;
+}
+
+const store = new Store();
+
+export const Context = createContext<StoreInterface>({ store });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{ store }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
