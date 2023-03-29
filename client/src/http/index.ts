54e5feb52e40ab3bc7b5eb2ab2388bd171c76361
ironyhouse <ironylait@gmail.com>
2023-03-29 21:33:59 +0200

    add logic to update refresh token

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { AuthResponse } from '../models/response/AuthResponse';
 
 export const API_ARL = 'http://localhost:5000/api';
 
@@ -13,5 +14,44 @@ $api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
+
+// update refresh and access tokens
+$api.interceptors.response.use(
+  // if token valid => return config
+  (config) => {
+    return config;
+  },
+  // else get new tokens and repeat previous request
+  async (error) => {
+    // save previous request
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      // error.config, error.config._isRetry - to avoid looping if statues is again 401
+      originalRequest._isRetry = true;
+      try {
+        // get new tokens
+        const response = await axios.get<AuthResponse>(`${API_ARL}/refresh`, {
+          withCredentials: true,
+        });
+
+        // set new token
+        localStorage.setItem('token', response.data.accessToken);
+
+        // repeat previous request
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.error('User is not authorized.');
+      }
+
+      // if status !== 401
+      throw error;
+    }
+  }
+);
 
 export default $api;
